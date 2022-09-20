32025a19e4ee81a7864e39def61e3521936eb785
Adam Gonzales <adamgonzales1@gmail.com>
2022-09-19 18:38:58 -0600

    feature: update css

--- src/routes/001-sign-up-page.jsx
+++ src/routes/001-sign-up-page.jsx
@@ -1,20 +1,23 @@
 import './001-sign-up-page.css'
+// https://dribbble.com/shots/16703354-Log-in-page-Untitled-UI
 
 export default function SignUpPage() {
   return (
-    <section className='sign-up-page-container'>
-      <div className='sign-up-content'>
+    <section className='sign-up__page'>
+      <div className='sign-up__content'>
         <div>
           <h1>Sign Up Now</h1>
-          <p>Enter Your Details</p>
-          <form className='sign-up-form' action=''>
+          <p>Welcome! Please enter your details.</p>
+          <form className='sign-up__form' action=''>
             <label htmlFor='email'>Email</label>
             <input id='email' name='email' type='email' />
             <label htmlFor='password'>Password</label>
             <input name='password' type='password' />
-            <div>
-              <input type='checkbox' />
-              Remember me for 30 days
+            <div className='row__inputs'>
+              <label htmlFor=''>
+                <input type='checkbox' />
+                Remember me for 30 days
+              </label>
               <a href='#'>Forgot Password</a>
             </div>
             <button>Sign in</button>
